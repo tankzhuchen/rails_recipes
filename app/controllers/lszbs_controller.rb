e0436388f373faeb1c3ea6e16cb361f21638ff7f
tank <tankzhuchen@163.com>
2019-08-02 11:08:27 +0800

    修改对比页面

--- app/controllers/lszbs_controller.rb
+++ app/controllers/lszbs_controller.rb
@@ -1,10 +1,14 @@
 class LszbsController < ApplicationController
 
- def index
-   @zhibiaos = Zhibiao.all.order("id DESC").page(params[:page]).per(10)
-@q = @zhibiaos.ransack(params[:q])
 
-@zhibiaos = @q.result
+      def index
+        @zhibiaos = Zhibiao.all.order("id DESC").page(params[:page]).per(20)
+        @q = @zhibiaos.ransack(params[:q])
+
+       @zhibiaos = @q.result
+
+     end
+
+
+
   end
-
-end
